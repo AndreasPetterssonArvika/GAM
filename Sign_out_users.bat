8d7dfd25e0000dafcd19d46ac24dc35bfd9ca6c9
AndreasPetterssonArvika <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-03-02 13:27:57 +0100

    Simplifying sign out script
    Simplifies the sign out script and removes the csv data file to solve issue #3.
    The data file is replaced by having the script contain one line per OU.

--- Sign_out_users.bat
+++ Sign_out_users.bat
@@ -1,17 +1,11 @@
-rem Logs out all users noted in the orgUnits.csv file
+rem Logs out all users in OUs
 rem
 rem Case #16054
-rem
-rem Format of the orgUnits.csv file
-rem
-rem orgUnitPath
-rem orgUnitPath=/<path1>
-rem orgUnitPath=/<path2>
-rem ...
-rem orgUnitPath=/<pathN>
 rem
 rem Links for reference
 rem https://github.com/jay0lee/GAM/wiki/SecurityExamples#signing-a-user-out
 rem https://github.com/jay0lee/GAM/wiki/BulkOperations#using-csv-files
 
-gam csv orgUnits.csv gam print users query ~orgUnitPath | gam csv - gam user ~primaryEmail signout
+gam print users query "orgUnitPath=/Students" | gam csv - gam user ~primaryEmail signout
+gam print users query "orgUnitPath=/Employees" | gam csv - gam user ~primaryEmail signout
+rem pause
