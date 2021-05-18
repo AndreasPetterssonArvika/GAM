ffd4e0596bffc4103fb9f98a467ce1d5709d0878
Andreas Pettersson <50955454+AndreasPetterssonArvika@users.noreply.github.com>
2021-05-18 12:49:54 +0200

    Create GamIncidentData.ps1
    Short script to gather some basic reports after an incident.

---
+++ GamIncidentData.ps1
@@ -0,0 +1,20 @@
+﻿# Hämtar rapporter och forwarding-adresser
+
+$userEmail = "username@domain"
+
+#Generera start- och slutdatum
+Write-Host "Generera start- och slutdatum"
+$startDayOffset = -30
+$timeFormat = 'yyyy-MM-ddTHH:MM:ss.fffZ'
+$startDate = (Get-Date).AddDays($startDayOffset) | Get-Date -Format $timeFormat
+$endDate = Get-Date -Format $timeFormat
+
+$reports = @('admin','login','drive')
+
+ForEach ($report in $reports) {
+    $gamReport = "gam report $report todrive user $userEmail start $startDate end $endDate"
+    Invoke-Expression $gamReport
+}
+
+$gamCmd = "gam user $userEmail show forwardingaddresses"
+Invoke-Expression $gamCmd
